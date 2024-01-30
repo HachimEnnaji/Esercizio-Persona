6ec79e644bc5827faed404253b736fd82c7d92b3
HachimEnnaji <ashennaji@hotmail.it>
2024-01-30 14:13:50 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio-Persona/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_Persona
+{
+    internal class Persona
+    {
+           public string Nome;
+           public string Cognome;
+           public int Eta;
+
+        public Persona(string nome,string cognome, int eta) {
+
+            this.Nome = nome;
+            this.Cognome = cognome;
+            this.Eta = eta;
+        }
+        public  string GetDettagli() {
+            //Console.WriteLine($"Nome :\t {this.Nome}\n cognome:\t {this.Cognome}\n ha:\t {this.Eta} anni");
+            return this.Nome + " " + this.Cognome + " " + this.Eta;
+        }
+        public string GetNome()
+        {
+            return this.Nome;
+        }
+        public string GetCognome()
+        {
+            return this.Cognome;
+        }
+        public int GetEta()
+        {
+            return this.Eta;
+        }
+    }
+}
---
+++ Esercizio-Persona/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Esercizio_Persona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Inserisci il nome:\t");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Inserisci il cognome:\t");
+            string cognome = Console.ReadLine();
+            Console.WriteLine("Inserisci l'eta':\t");
+            int eta;
+            while (!int.TryParse(Console.ReadLine(), out eta))
+            {
+
+                Console.WriteLine("Errore il numero inserito non è corretto, prego reinserire!");
+            }
+
+            Persona omar = new Persona(nome, cognome, eta);
+            Console.WriteLine(omar.GetDettagli());
+            Console.ReadKey();
+        }
+    }
+}
